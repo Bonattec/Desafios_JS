1a897f94fbae3ede76e9ee6a6d0a886e660cdb04
Bonattec <gabrielpdsbonato@gmail.com>
2026-06-15 21:52:22 -0300

    Tentativa Desafio 03

---
+++ Desafio04/loja.js
@@ -0,0 +1,19 @@
+/*
+DESAFIO 04 - LOJA TECH
+
+Uma loja de informática possui vários produtos cadastrados.
+
+Crie um array de objetos contendo:
+- nome
+- preco
+
+Utilize uma estrutura de repetição para mostrar apenas
+os produtos que custam mais de R$ 100,00
+
+Exiba o nome dos produtos encontrados
+*/
+
+let nomeProdutos = ["Placa-mãe", "SSD", "Processador", "Mouse", "Microfone"];
+let precoProdutos = [700, 350, 1200, 70, 90];
+    for (precoProdutos  >  100 ; precoProdutos, nomeProdutos)  {
+        console.log(nomeProdutos + precoProdutos)}
