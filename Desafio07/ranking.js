128b00ed803a06340acf66910326d12e94aea8c9
Bonattec <gabrielpdsbonato@gmail.com>
2026-06-22 21:32:30 -0300

    Desafio07

---
+++ Desafio07/ranking.js
@@ -0,0 +1,21 @@
+/*
+DESAFIO 07 - RANKING DE JOGADORES
+
+Uma competição precisa exibir a calssificação dos participantes.
+
+Crie um array contendo o nome de 4 jogadores.
+
+Utilize uma estrutura de repetição para mostrar:
+
+1° Jogador
+2° Jogador
+3° Jogador
+4° Jogador
+
+A posição deve ser gerada automaticamente pelo sistema.
+*/
+const jogadores = ["Gabriel", "Lucas", "Luiz", "Leonardo"]
+    for(i = 0; i < jogadores.length; i++) {
+        console.log(jogadores[i])
+        
+    }
